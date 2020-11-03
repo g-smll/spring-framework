a94f693097cd637f843ef346612ac3de6364cbbd
NPZ\chengang <chengang2017>
2020-11-03 17:33:09 +0800

    SPRING 源码成功编码

--- spring-beans/src/main/java/org/springframework/beans/factory/Aware.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/Aware.java
@@ -21,7 +21,6 @@ package org.springframework.beans.factory;
  * Spring container of a particular framework object through a callback-style method.
  * The actual method signature is determined by individual subinterfaces but should
  * typically consist of just one void-returning method that accepts a single argument.
- *
  * <p>Note that merely implementing {@link Aware} provides no default functionality.
  * Rather, processing must be done explicitly, for example in a
  * {@link org.springframework.beans.factory.config.BeanPostProcessor}.
